8296096e1ed7c03c0b8e5595b1d5ffa40d69b108
GRBRacoon <chldusgh0497@gmail.com>
2023-09-30 19:13:26 +0900

    Gradle추가 @Lombok 추가

---
+++ src/main/java/kr/ac/jbnu/se/tetris/controllers/WebSocketController.java
@@ -0,0 +1,23 @@
+package kr.ac.jbnu.se.tetris.controllers;
+
+import java.util.logging.Logger;
+
+
+public class WebSocketController {
+
+    private Logger logger = Logger.getLogger(WebSocketController.class.getName());
+    private KeyInputController inputController1;
+    private KeyInputController inputController2;
+    public static WebSocketController webSocketController
+            = new WebSocketController();
+
+    private WebSocketController(){logger.info("WebSocketController start");}
+
+    public void setInputController1(KeyInputController inputController) {
+        this.inputController1= inputController;
+    }
+
+    public void setInputController2(KeyInputController inputController2) {
+        this.inputController2 = inputController2;
+    }
+}
