ca0a2d8319f3fce9a3b8fb314e2d170622ae3418
GRBRacoon <chldusgh0497@gmail.com>
2023-09-30 21:55:18 +0900

    로그인, 회원가입 메서드 완료

--- src/main/java/kr/ac/jbnu/se/tetris/TetrisStartGame.java
+++ src/main/java/kr/ac/jbnu/se/tetris/TetrisStartGame.java
@@ -1,14 +1,16 @@
 package kr.ac.jbnu.se.tetris;
 
 
+import kr.ac.jbnu.se.tetris.views.Page;
 import kr.ac.jbnu.se.tetris.views.TetrisFrame;
 import kr.ac.jbnu.se.tetris.views.TetrisFrameForTwo;
 
 public class TetrisStartGame {
     public
     static void main(String[] args) {
-        TetrisFrame game = new TetrisFrameForTwo();
-        game.setLocationRelativeTo(null);
-        game.init();
+        //TetrisFrame game = new TetrisFrameForTwo();
+        //game.setLocationRelativeTo(null);
+        //game.init();
+        new Page();
     }
 }
