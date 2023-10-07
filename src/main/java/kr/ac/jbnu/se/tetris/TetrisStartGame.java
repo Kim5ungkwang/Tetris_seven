4016355b54014efd1f8420abd5d425da20578df5
gwang6736 <ksg6736@gmail.com>
2023-10-08 04:03:47 +0900

    mainPage 작성중

--- src/main/java/kr/ac/jbnu/se/tetris/TetrisStartGame.java
+++ src/main/java/kr/ac/jbnu/se/tetris/TetrisStartGame.java
@@ -1,14 +1,18 @@
 package kr.ac.jbnu.se.tetris;
 
 
+import kr.ac.jbnu.se.tetris.controllers.MainPageController;
 import kr.ac.jbnu.se.tetris.controllers.TetrisFrameController;
 
+import java.io.IOException;
+
 public class TetrisStartGame {
-    public
-    static void main(String[] args) {
-        TetrisFrameController game = new TetrisFrameController();
-        game.setLocationRelativeTo(null);
-        game.init();
+    public static void main(String[] args) throws IOException {
+        //TetrisFrameController game = new TetrisFrameController();
+        //game.setLocationRelativeTo(null);
+        //game.init();
+        MainPageController mainPage = new MainPageController();
+        mainPage.setLocationRelativeTo(null);
         //new Page();
     }
 }
--- src/main/java/kr/ac/jbnu/se/tetris/controllers/MainPageController.java
+++ src/main/java/kr/ac/jbnu/se/tetris/controllers/MainPageController.java
@@ -1,9 +1,57 @@
 package kr.ac.jbnu.se.tetris.controllers;
 
-import com.sun.tools.javac.Main;
+import kr.ac.jbnu.se.tetris.models.MainPageModel;
 
-public class MainPageController {
-    public MainPageController(){
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 
+public class MainPageController extends JFrame {
+    JPanel background;
+    static private final int MAINPAGE_SIZE_WIDTH = 1280;
+    static private final int MAINPAGE_SIZE_HEIGHT = 720;
+    MainPageModel mainPageModel;
+    JPanel backgroundPanel;
+    JButton localPlayBt, multiPlayBt, onlinePlayBt, settingBt, singlePlayBt, sprintBt, tutorialBt;
+    public MainPageController() throws IOException {
+        setSize(new Dimension(MAINPAGE_SIZE_WIDTH, MAINPAGE_SIZE_HEIGHT));
+        setLayout(null);
+        mainPageModel = new MainPageModel();
+        buttonInit();
+
+        backgroundPanel = mainPageModel.getBackgroundPanel();
+        add(backgroundPanel);
+        backgroundPanel.setSize(MAINPAGE_SIZE_WIDTH,MAINPAGE_SIZE_HEIGHT);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
     }
+
+    public void paint(Graphics g){
+        super.paint(g);
+    }
+
+    public void buttonInit(){
+        int buttonX = MainPageModel.BUTTON_X;
+        int buttonY = MainPageModel.BUTTON_Y;
+        int buttonWidth = MainPageModel.BUTTON_WIDTH;
+        int buttonHeight = MainPageModel.BUTTON_HEIGHT;
+
+        localPlayBt = mainPageModel.getLocalPlayBt();
+        multiPlayBt = mainPageModel.getMultiPlayBt();
+        onlinePlayBt = mainPageModel.getOnlineBt();
+        settingBt = mainPageModel.getSettingBt();
+        singlePlayBt = mainPageModel.getSinglePlayBt();
+        sprintBt = mainPageModel.getSprintBt();
+        tutorialBt = mainPageModel.getTutorialBt();
+
+        localPlayBt.setBounds(buttonX, buttonY, buttonWidth, buttonHeight);
+        multiPlayBt.setBounds(buttonX, buttonY + buttonHeight + 30, buttonWidth, buttonHeight);
+        settingBt.setBounds(buttonX, buttonY +  buttonHeight*2 + 60, buttonWidth, buttonHeight);
+
+        add(localPlayBt);
+        add(multiPlayBt);
+        add(settingBt);
+    }
+
 }
