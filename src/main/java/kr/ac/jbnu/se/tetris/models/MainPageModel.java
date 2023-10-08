6d497abf53e57a60e766ee740e4dbc000aaf5fe5
gwang6736 <ksg6736@gmail.com>
2023-10-08 19:28:04 +0900

    메인 매뉴 추가

--- src/main/java/kr/ac/jbnu/se/tetris/models/MainPageModel.java
+++ src/main/java/kr/ac/jbnu/se/tetris/models/MainPageModel.java
@@ -1,13 +1,9 @@
 package kr.ac.jbnu.se.tetris.models;
 
-import kr.ac.jbnu.se.tetris.controllers.MainPageController;
 import lombok.Getter;
 
 import java.awt.*;
-import java.io.File;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
-import javax.imageio.ImageIO;
 import javax.swing.*;
 
 import static javax.imageio.ImageIO.read;
@@ -18,41 +14,43 @@ public class MainPageModel{
     static public final int BUTTON_X = 518;
     static public final int BUTTON_Y = 290;
     @Getter
-    private JPanel backgroundPanel;
+    private JPanel backgroundPanel, multiPlayPagePanel, singlePlayPagePanel;    //백그라운드, 멀티플레이 누르면나오는 패널, 싱글플레이 누르면 나오는 패널
     @Getter
-    static ImageIcon backGroundImg, multiPlayImg, settingImg, localPlayImg, onlineImg, sprintImg, tutorialImg, singlePlayImg;
+    static ImageIcon backGroundImg, multiPlayImg, settingImg, singlePlayImg;
     @Getter
-    JButton singlePlayBt, multiPlayBt, settingBt, localPlayBt, onlineBt, sprintBt, tutorialBt;
+    JButton singlePlayBt, multiPlayBt, settingBt;
 
 
     public MainPageModel() throws IOException {
 
-        backGroundImg = new ImageIcon("source/image/background/mainpagebackground.png");
+        backGroundImg = new ImageIcon("source/image/background/mainpagebackground.png");    //버튼 이미지 아이콘 위치
         multiPlayImg = new ImageIcon("source/image/button/multiplay.png");
         settingImg = new ImageIcon("source/image/button/setting.png");
-        localPlayImg = new ImageIcon("source/image/button/localplay.png");
-        onlineImg = new ImageIcon("source/image/button/onlineplay.png");
-        sprintImg = new ImageIcon("source/image/button/sprint.png");
-        tutorialImg = new ImageIcon("source/image/button/tutorial.png");
         singlePlayImg = new ImageIcon("source/image/button/singleplay.png");
 
-        singlePlayBt = new JButton(singlePlayImg);
+        singlePlayBt = new JButton(singlePlayImg);  //버튼 제작
         multiPlayBt = new JButton(multiPlayImg);
         settingBt = new JButton(settingImg);
-        localPlayBt = new JButton(localPlayImg);
-        onlineBt = new JButton(onlineImg);
-        sprintBt = new JButton(sprintImg);
-        tutorialBt = new JButton(tutorialImg);
 
-        backgroundPanel = new JPanel(){
+        backgroundPanel = new JPanel(){ //배경 패널 설정
             public void paintComponent(Graphics g){
                 g.drawImage(MainPageModel.getBackGroundImg().getImage(), 0, 0, null);
                 setOpaque(false);
                 super.paintComponent(g);
             }
         };
-        backgroundPanel.setSize(1820, 720);
+        backgroundPanel.setSize(1280, 720);
+        singlePlayPagePanel = new JPanel();
+        multiPlayPagePanel = new JPanel();
     }
+
+
+    public void multiPlayPagePanelInit(){
+        multiPlayPagePanel.setSize(1280, 720);
+        multiPlayPagePanel.setBackground(new Color(65, 65, 65, 179));
+        multiPlayBt.setBounds(BUTTON_X, BUTTON_Y, BUTTON_WIDTH,BUTTON_HEIGHT);
+    }
+
 
 
 
