7bbe339f83a642b5843cb73a07688ba8adffe7cb
intoMaven <yesyesj0426@gmail.com>
2023-10-08 20:18:14 +0900

    튜토리얼 수정

--- src/main/java/kr/ac/jbnu/se/tetris/controllers/TutorialController.java
+++ src/main/java/kr/ac/jbnu/se/tetris/controllers/TutorialController.java
@@ -1,9 +1,7 @@
 package kr.ac.jbnu.se.tetris.controllers;
 
 import kr.ac.jbnu.se.tetris.ShapeData;
-import kr.ac.jbnu.se.tetris.models.BoardModel;
-import kr.ac.jbnu.se.tetris.models.KeyInput;
-import kr.ac.jbnu.se.tetris.models.TutorialModel;
+import kr.ac.jbnu.se.tetris.models.*;
 import lombok.Getter;
 
 import java.util.Timer;
@@ -18,6 +16,10 @@ import static kr.ac.jbnu.se.tetris.models.TutorialModel.tutorialSteps;
 public class TutorialController extends BoardController {
     @Getter
     final private TutorialModel tutorialModel;
+    Piece piece;
+    TutorialModel brickQueueManager;
+
+
     private Timer timer;
 
     public TutorialController(PlayerPageController parent, KeyInput input){
@@ -52,48 +54,30 @@ public class TutorialController extends BoardController {
     }
 
     public void moveToNextStep() {
-        if (tutorialModel.getCurrentStepIndex() < tutorialSteps.length - 1) {
+        if (tutorialModel.getCurrentStepIndex() < tutorialSteps.length - 1 && tutorialModel.getCurrentStepIndex()!=4) {
             tutorialModel.plusCurrnetStepIndex();
             displayCurrentStep(); // 다음 튜토리얼을 게임 보드에 표시
         } else if (tutorialModel.getCurrentStepIndex() == 4) {
             drawBackgroundblock();
+
         }else {
             // 튜토리얼 마지막 단계 도달한 경우 로직 추가
             start();
         }
     }
-    private void performStepAciton() {
-        switch(tutorialModel.getCurrentStepIndex()) {
-            case 0:
-        }
-    }
+
     private void drawBackgroundblock() {
-        int[][] tutorialBackgroundCoords = ShapeData.COORDS_TABLE[ShapeData.Tetrominoes.TutorialBackground.ordinal()];
-
-        // 배치할 TutorialBackground 블록 단 수
-        int numberOfRows = 3;
-        int numberOfColumns = 7;
-        int targetColumn = 0;
-
-        for (int row = 0; row < numberOfRows; row++) {
-            for (int col = 0; col < numberOfColumns; col++) {
-                for (int i = 0; i < tutorialBackgroundCoords.length; ++i) {
-                    int x = tutorialBackgroundCoords[i][0];
-                    int y = tutorialBackgroundCoords[i][1];
-
-                    // 각 블록의 초기 위치 계산
-                    int initialX = col * tutorialBackgroundCoords.length; // 각 블록 간의 가로 간격 조절
-                    int initialY = BoardModel.getBoardHeight() - (row * tutorialBackgroundCoords[0].length); // 각 블록 간의 세로 간격 조절
-
-                    int boardX = initialX + x;
-                    int boardY = initialY + y;
-                    // 1열 12행에 해당하는 좌표의 블록을 NoShape로 설정
-                    if (x == targetColumn && y == 0) {
-                        getBoardModel().setboard(x + y * BoardModel.getBoardWidth(), ShapeData.Tetrominoes.NoShape);
-                    }
-                    getBoardModel().setboard(boardX + boardY * BoardModel.getBoardWidth(), ShapeData.Tetrominoes.TutorialBackground);
-                }
+        for (int i = 0; i < BoardModel.getBoardHeight() * BoardModel.getBoardWidth(); ++i)
+            getBoardModel().setboard(i, ShapeData.Tetrominoes.NoShape);
+        for(int x = 3; x<BoardModel.getBoardWidth(); x++){
+            for(int y = 0; y < 12; y++){
+                getBoardModel().setboard(x + y * BoardModel.getBoardWidth(), ShapeData.Tetrominoes.TutorialBackground);
+                getBoardModel().setboard(3+11*BoardModel.getBoardWidth(), ShapeData.Tetrominoes.NoShape);
             }
         }
+    }
+    public void drawNextTutorialBlock(){
+        piece = new Piece();
+        ShapeData.Tetrominoes block = tutorialModel.getTutorialBlock(tutorialModel.getCurrentBrickIndex());
     }
 }
--- src/main/java/kr/ac/jbnu/se/tetris/models/TutorialModel.java
+++ src/main/java/kr/ac/jbnu/se/tetris/models/TutorialModel.java
@@ -4,10 +4,16 @@ import kr.ac.jbnu.se.tetris.ShapeData;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.awt.*;
+
 public class TutorialModel{
 
     @Getter
     private int currentStepIndex = 0; //현재 튜토리얼 스텝
+    @Getter
+    private int currentBrickIndex = 0; // 현재 블럭 인덱스
+    public static BrickQueueManager tutorialQueue;
+    private ShapeData.Tetrominoes[] tutorialBlocks;
     public static final String[] tutorialSteps = {
             "<html>Step 1: 테트리스 규칙<br>게임의 룰은 내려오는 블록을 한 줄이 꽉 차도록 합니다.<br> 그 줄은 사라지고 이것을 반복하면서 블록이 맨 위까지 안 쌓이게 끝까지 버티면서 플레이하는 게임입니다.</html>",
             "<html>Step 2: 블록 이동하기<br>블록을 움직이려면 A, D 키를 누르세요</html>",
@@ -17,16 +23,25 @@ public class TutorialModel{
             "튜토리얼 끝! 게임을 시작합니다!"
     };
     public TutorialModel(){
-        currentStepIndex = 0;
+        tutorialQueue = new BrickQueueManager();
+        tutorialBlocks = new ShapeData.Tetrominoes[6];
+        tutorialBlocks[0] = ShapeData.Tetrominoes.LShape;
+        tutorialBlocks[1] = ShapeData.Tetrominoes.SShape;
+        tutorialBlocks[2] = ShapeData.Tetrominoes.SquareShape;
+        tutorialBlocks[3] = ShapeData.Tetrominoes.ZShape;
+        tutorialBlocks[4] = ShapeData.Tetrominoes.TShape;
+        tutorialBlocks[5] = ShapeData.Tetrominoes.TShape;
+    }
+    public ShapeData.Tetrominoes getTutorialBlock(int step){
+        return tutorialBlocks[currentBrickIndex++];
     }
 
     public void plusCurrnetStepIndex(){
-       this.currentStepIndex++;
+        this.currentStepIndex++;
     }
 
     public void resetCurrentStepIndex() {
         currentStepIndex = 0;
     }
 }
-
 
